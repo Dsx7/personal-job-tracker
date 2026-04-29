c637cccda09a19389352284e16621be5234102ca
Dsx7 <dsxspa@gmail.com>
2026-04-29 13:29:25 +0600

    fixed google user schema validation

--- models/User.ts
+++ models/User.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: { type: String, required: false },
+  password: { type: String, required: false },
 }, { timestamps: true });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
